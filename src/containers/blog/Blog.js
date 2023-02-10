4e1e8ebb3e1d73b9ab0476de91bc35050af10c9d
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-02-10 22:27:20 +0300

    Articles

--- src/containers/blog/Blog.js
+++ src/containers/blog/Blog.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import './blog.css';
+import { Article } from '../../components';
+import {blog01,blog02,blog03,blog04,blog05} from  './imports'
+
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="gpt3__blog section__padding" id='blog'>
+      <div className="gpt3__blog-heading">
+        <h1 className='gradient__text'>A lot is Happening, we are Blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} date="Sep 26 2022" title="GPT-3 and Open Ai is the future. Let us explore how it works?"/>
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} date="Sep 26 2022" title="GPT-3 and Open Ai is the future. Let us explore how it works?" />
+          <Article imgUrl={blog03} date="Sep 26 2022" title="GPT-3 and Open Ai is the future. Let us explore how it works?" />
+          <Article imgUrl={blog04} date="Sep 26 2022" title="GPT-3 and Open Ai is the future. Let us explore how it works?" />
+          <Article imgUrl={blog05} date="Sep 26 2022" title="GPT-3 and Open Ai is the future. Let us explore how it works?" />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import './Possibility.css'
+import possibilityImage from '../../assets/possibility.png'
+
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibilty" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early access to get started</h4>
+        <h1 className="gradient__text">
+          The Possibilty are beyond your imagination
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid rem accusamus obcaecati et molestias labore minima exercitationem necessitatibus ad est.</p>
+        <h4>Request Early Access to get started</h4>
+      </div>
+    </div>
   )
 }
 
